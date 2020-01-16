81f578460992868e99551fa82cc0f09f062fae84
hencore <hencore.dev@protonmail.com>
2020-01-16 16:53:28 +0100

    1er ajout de la map

---
+++ map.js
@@ -0,0 +1,18 @@
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+var lat = 43.60426;
+var lon = 1.44367;
+var macarte = null;
+// Fonction d'initialisation de la carte
+function initMap() {
+    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+    macarte = L.map('map').setView([lat, lon], 11);
+    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données									minZoom: 1,
+        maxZoom: 20
+    }).addTo(macarte);
+}
+window.onload = function(){
+// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+initMap();
+};
