1bb8e0b64cd0b4cdb086bc87c86e50924bf1f74e
henc0re <hencore.dev@protonmail.com>
2020-03-13 17:49:21 +0100

    retouche avant la session

--- Javascript/map.js
+++ Javascript/map.js
@@ -1,22 +1,27 @@
-//let lat = 43.60426;
-//let lon = 1.44367;
+let lat = 43.60426;
+let lon = 1.44367;
+//let minZoom = 10;
+//let maxZoom = 25;
 //let macarte = null;
-//let zoom = [10, 25];
 
 class Carte {
-  constructor (lat, lon, minZoom, maxZoom) {
+  constructor () {
+    //let lat = 43.60426;
+    //let lon = 1.44367;
+    //let minZoom = 10;
+    //let maxZoom = 25;
+    //let macarte = null;
     this.lat = lat;
     this.lon = lon;
-    this.macarte = null;
-    this.minZoom = minZoom;
-    this.maxZoom = maxZoom;
+    let macarte = null;
+    //this.minZoom = minZoom;
+    //this.maxZoom = maxZoom;
   }
     initmap() {
-      this.macarte = L.map('map',{scrollWheelZoom: false})
-      .setView([this.lat, this.lon, 11]);
+      this.macarte = L.map('map',{scrollWheelZoom: false}).setView([this.lat, this.lon, 11]);
       L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',{
-        minZoom = 10,
-        maxZoom = 25
+        /*this.*/minZoom: 10,
+        /*this.*/maxZoom: 25
       }).addTo(this.macarte);
     }
 }
@@ -26,3 +31,5 @@ const mapToulouse = new Carte(43.60426, 1.44367, null);
 window.onload = function(){
 mapToulouse.initmap();
 };
+
+//let marker = L.marker([mapToulouse.lat, mapToulouse .lon]).addTo(mapToulouse.macarte);
