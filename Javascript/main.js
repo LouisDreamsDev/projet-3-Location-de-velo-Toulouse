8fa6c95a71bd0a9f66ba4e196d6a20f2f4418815
henc0re <hencore.dev@protonmail.com>
2020-03-21 10:52:26 +0100

    21/03/20 test main

---
+++ Javascript/main.js
@@ -0,0 +1,11 @@
+// Slider
+let playButton = document.getElementById('play-button');
+let stopButton = document.getElementById('stop-button');
+//let images = document.getElementsByClassName("slider-image");
+let sliderIndex = 0;
+let chrono = 0;
+const slider1 = new Slider(document.getElementsByClassName("slider-image"));
+
+// Map
+
+
